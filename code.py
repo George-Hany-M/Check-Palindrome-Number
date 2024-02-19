f03cfc01321af0a74771b89a1cd6c3b865400f68
George Hany Milad <92086186+George-Hany-M@users.noreply.github.com>
2024-02-19 10:26:07 +0200

    Add files via upload

---
+++ code.py
@@ -0,0 +1,22 @@
+# George Hany
+# Write a program to check if the given number is a palindrome number.
+# A palindrome number is a number that is the same after reverse. For example, 545, is the palindrome numbers.
+
+# input the number from the user
+num = input('The number is : ')
+
+# boolean to chech if the number is palindrome or not ?!
+flag = True
+
+length = len(num)
+limit = length // 2
+for index in range(limit): # 0 1 2 3 4 5
+    if num[index] != num[length - index - 1]:
+        flag = False
+        break
+    
+if flag:
+    print('Yes, given number is palindrome number')
+else:
+    print('No,  given number is not palindrome number')
+    
